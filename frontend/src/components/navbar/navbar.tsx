78cdab0b9533607cf677eb335ec1bc96ba6769bd
maria-afteni <marianaafteni2003@gmail.com>
2025-06-22 02:29:43 +0300

    feat(styles-navbar): add styles for navbar

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -14,18 +14,30 @@ const Navbar = () => {
 	const { user } = useAuth();
 
 	return (
-		<nav className="navbar bg-accent flex justify-between items-center py-4 px-6">
-			<Link to="/">Summer FAF Hackathon 2025</Link>
+		<nav className="sticky top-0 z-50 bg-gradient-to-br from-purple-600 to-blue-600 ">
+			<div className="px-4 sm:px-6 lg:px-20">
+				<div className="flex items-center justify-between h-16 ">
+			<Link to="/" className="group font-bold text-2xl text-white font-[Apple Color Emoji]">ContributeIT</Link>
 			{user ? (
 				<ProfileDropdown user={user} onLogout={() => {}} />
 			) : (
-				<Button variant={"default"} onClick={handleGitHubLogin}>
+				<Button onClick={handleGitHubLogin}
+				className="group relative overflow-hidden
+						bg-gradient-to-r from-purple-300 to-blue-300 text-black text-base hover:from-gray-800 hover:to-gray-700
+						hover:text-white font-semibold
+						px-6 py-2.5 rounded-xl
+						shadow-lg hover:shadow-xl
+						border border-gray-700 hover:border-gray-600
+						transform hover:scale-105 transition-all duration-300">
 					<span>Login with</span>
 					<span>
 						<FaGithub />
 					</span>
 				</Button>
 			)}
+			
+			</div>
+			</div>
 		</nav>
 	);
 };
