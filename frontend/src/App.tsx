9a7985855a444d3b7568d7d50fc1e42cee0d792f
Sevastian Bajenov <sevabaj@gmail.com>
2025-06-21 16:53:06 +0300

    setup: add routing and axios config.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
+import PageLayout from './routes/page-layout'
+import { AxiosInterceptorWrapper } from './utils/axios'
 
 function App() {
   return (
     <>
-      <div>Project works</div>
+      <BrowserRouter>
+        <AxiosInterceptorWrapper>
+          <Route index element="<div></div>" />
+          <Route path="/register" element="<div></div>" />
+          <Route path="/login" element="<div></div>" />
+          <Route path="*" element={<PageLayout />} />
+        </AxiosInterceptorWrapper>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,67 @@
+import axios, {
+  AxiosError,
+  type AxiosResponse,
+  type InternalAxiosRequestConfig,
+} from "axios";
+import { type ReactNode, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { UserRoleContext } from "../context/UserRoleContext";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const AxiosInterceptorWrapper = ({ children }: { children: ReactNode }) => {
+  // const userRoleContextProps = useContext(UserRoleContext);
+  const navigate = useNavigate();
+
+  const [interceptorsSet, setInterceptorsSet] = useState(false);
+
+  useEffect(() => {
+    const requestInterceptor = (config: InternalAxiosRequestConfig) => {
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    };
+
+    const responseInterceptor = (response: AxiosResponse) => {
+      return response;
+    };
+
+    const errorInterceptor = (error: AxiosError) => {
+      if (error.response && error.response.status === 401) {
+        // userRoleContextProps?.setUserRole("");
+        // userRoleContextProps?.setUserCredentials("");
+        navigate("/");
+      }
+      return Promise.reject(error);
+    };
+
+    const reqInterceptor = api.interceptors.request.use(
+      requestInterceptor,
+      (error) => Promise.reject(error)
+    );
+
+    const resInterceptor = api.interceptors.response.use(
+      responseInterceptor,
+      errorInterceptor
+    );
+
+    setInterceptorsSet(true);
+
+    return () => {
+      api.interceptors.request.eject(reqInterceptor);
+      api.interceptors.response.eject(resInterceptor);
+    };
+  }, [navigate]);
+
+  return interceptorsSet && children;
+};
+
+export default api;
+export { AxiosInterceptorWrapper };
