d99ee1334f767ea887f6bd7d640c6f5f45ba5c75
maria-afteni <marianaafteni2003@gmail.com>
2025-06-21 19:50:08 +0300

    feat(profile): add jwt decoder in profile page

---
+++ frontend/src/pages/profile.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import Navbar from '../components/navbar/navbar';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+interface JwtPayload {
+  username: string;
+}
+
+const getToken = () => localStorage.getItem('token');
+
+const getUsername = (token: string): string | null => {
+  try {
+    const decoded = jwtDecode<JwtPayload>(token);
+    return decoded.username;
+  } catch {
+    return null;
+  }
+};
+
+const Profile: React.FC = () => {
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = getToken();
+    if (!token) return;
+
+    const name = getUsername(token);
+    setUsername(name);
+  }, []);
+
+
+  return (
+    <div>
+			<Navbar />
+	
+      <h1>Welcome, {username}!</h1>
+    </div>
+  );
+};
+export default Profile;
