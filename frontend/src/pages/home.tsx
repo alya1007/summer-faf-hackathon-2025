07803af0a5fdf201d8a5bd630e72f6f40e73d8e2
alya1007 <alexandra.konjevic@isa.utm.md>
2025-06-22 01:51:53 +0300

    feat(repos-data): add repos fetching in home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,44 +1,30 @@
+import { useEffect } from "react";
 import FiltersSection from "../components/filter/filters-section";
 import ReposList from "../components/repos-list/repos-list";
-const mockData = {
-	count: 3,
-	next: null,
-	previous: null,
-	results: [
-	  {
-		name: "TestRepo1",
-		url: "http://github.com/test-user/test-repo-1",
-		stars: 5,
-		forks: 2,
-		langs: ["python"],
-		domains: ["backend"],
-		good_first: false,
-	  },
-	  {
-		name: "TestRepo0",
-		url: "http://github.com/test-user/test-repo-0",
-		stars: 1,
-		forks: 1,
-		langs: ["c#"],
-		domains: ["game_dev"],
-		good_first: true,
-	  },
-	  {
-		name: "TestRepo2",
-		url: "http://github.com/test-user/test-repo-2",
-		stars: 1,
-		forks: 2,
-		langs: ["js"],
-		domains: ["frontend"],
-		good_first: true,
-	  },
-	],
-  };
+
 const Home = () => {
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					`${import.meta.env.VITE_API_URL}/repos/search/`
+				);
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.json();
+				console.log(data);
+			} catch (error) {
+				console.error("Fetch error:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
 	return (
 		<div className="flex flex-1 m-10 gap-10">
 			<FiltersSection />
-			<ReposList data={mockData}/>
+			<ReposList data={mockData} />
 		</div>
 	);
 };
